19ddcb1a5e591af17b25025c420a781fd4c4fd63
Bas Kaal <3askaal@gmail.com>
2022-02-26 03:17:43 +0100

    fix styling for smallest screen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -19,7 +19,7 @@ const App = () => {
     <ThemeProvider
       theme={{
         ...theme,
-        rootFontSizes: ['8px', '14px', '20px'],
+        rootFontSizes: ['10px', '14px', '20px'],
         fonts: {
           ...theme.fonts,
           ...fonts,
